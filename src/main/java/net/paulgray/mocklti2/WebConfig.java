d9f9c39ec19358a8eeb281b18813b430d8a44d3a
Paul Gray <pfbgray@gmail.com>
2014-07-22 17:39:59 -0400

    Fixing dependencies

--- src/main/java/net/paulgray/mocklti2/WebConfig.java
+++ src/main/java/net/paulgray/mocklti2/WebConfig.java
@@ -6,11 +6,7 @@
 
 package net.paulgray.mocklti2;
 
-import java.util.List;
-import net.paulgray.lmsrest.web.DefaultContextUserArgumentResolver;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
