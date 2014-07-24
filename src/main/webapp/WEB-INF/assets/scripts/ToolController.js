49fb92d2b3739a69555722408bdad00dd2b6fd38
Paul Gray <pfbgray@gmail.com>
2014-07-24 14:49:23 -0400

    Removing separate tool templates

--- src/main/webapp/WEB-INF/assets/scripts/ToolController.js
+++ src/main/webapp/WEB-INF/assets/scripts/ToolController.js
@@ -5,24 +5,61 @@
  */
 
 app.controller('ToolsController', ['$scope', '$http', function($scope, $http) {
+    
+    var AddedTool = function(tool){
+        tool.button = {
+            label:"Register Tool",
+            icon:"fa-wrench",
+            onClick:function(){
+                alert('added tool');
+            }
+        };
+        tool.liClass = 'list-group-item-warning';
+        return tool;
+    };
+    var RegisteredTool = function(tool){
+        tool.button = {
+            label:"Launch Tool",
+            icon:"fa-rocket",
+            onClick:function(){
+                alert('registered tool');
+            }
+        };
+        tool.liClass = 'list-group-item-success';
+        return tool;
+    };
+    var FailedTool = function(tool){
+        tool.button = {
+            label:"Re-Register Tool",
+            icon:"fa-wrench",
+            onClick:function(){
+                alert('failed tool');
+            }
+        };
+        tool.liClass = 'list-group-item-danger';
+        return tool;
+    }
+        
+        
+        
     $scope.loading = true;
     $http({method: 'GET', url: 'api/tools'})
     .success(function(data, status, headers, config) {
-        $scope.tools = data;
+        $scope.tools = [];
+        for(var i=0; i<data.length; i++){
+            if(data[i].state === 'registered'){
+                $scope.tools.push(new RegisteredTool(data[i]));
+            } else if (data[i].state === 'added'){
+                $scope.tools.push(new AddedTool(data[i]));
+            } else if (data[i].state === 'failed'){
+                $scope.tools.push(new FailedTool(data[i]));
+            }
+        }
         $scope.loading = false;
     })
     .error(function(data, status, headers, config) {
         $scope.error = true;
         $scope.loading = false;
     });
-    $scope.getLiClassForState = function(state){
-        if(state === 'registered'){
-            return 'list-group-item-success';
-        } else if (state === 'added'){
-            return 'list-group-item-warning';
-        } else if (state === 'failed'){
-            return 'list-group-item-danger';
-        }
-    }
     
 }]);
