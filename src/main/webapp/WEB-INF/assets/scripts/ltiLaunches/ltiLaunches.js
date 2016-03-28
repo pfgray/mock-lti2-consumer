16e115b25e614c36e2a0b760d907ca412d431e42
Paul Gray <pgray@learningobjects.com>
2016-03-28 10:23:16 -0400

    Scoping launch params to each section

--- src/main/webapp/WEB-INF/assets/scripts/ltiLaunches/ltiLaunches.js
+++ src/main/webapp/WEB-INF/assets/scripts/ltiLaunches/ltiLaunches.js
@@ -2,8 +2,11 @@ app.directive('ltiLaunches', function() {
   return {
     templateUrl: 'assets/scripts/ltiLaunches/lti-launches.html',
     link: function(scope, element, attrs) {
-        scope.launch = {};
+      scope.launch = {};
 
+      scope.launch.tool = {};
+      scope.launch.user = {};
+      scope.launch.context = {};
     }
   };
 });
