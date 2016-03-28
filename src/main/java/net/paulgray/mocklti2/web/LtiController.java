d22f8dd6e9e9cb99a576c889faaee9e368069da3
Paul Gray <pgray@learningobjects.com>
2016-03-28 10:14:24 -0400

    Fixing signed launch endpoint

--- src/main/java/net/paulgray/mocklti2/web/LtiController.java
+++ src/main/java/net/paulgray/mocklti2/web/LtiController.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.util.Map;
+
 /**
  * Created by paul on 3/27/16.
  */
@@ -17,7 +19,7 @@ public class LtiController {
     LtiSigner ltiSigner;
 
     @RequestMapping(value = "/api/signedLaunch", method = RequestMethod.POST)
-    public LtiLaunchRequest getSignedLaunch(UnsignedLtiLaunchRequest request) throws LtiSigningException {
+    public Map<String, String> getSignedLaunch(UnsignedLtiLaunchRequest request) throws LtiSigningException {
         return ltiSigner.signParameters(
                 request.getLaunchParameters(),
                 request.getKey(),
