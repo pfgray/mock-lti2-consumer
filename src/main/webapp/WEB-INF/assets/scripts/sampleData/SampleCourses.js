3c9345730fb092272d826d2499b0ad42f6ac5774
Paul Gray <pfbgray@gmail.com>
2018-09-18 20:49:57 -0400

    Renaming resource_title to resource_link_title, fixes #4

--- src/main/webapp/WEB-INF/assets/scripts/sampleData/SampleCourses.js
+++ src/main/webapp/WEB-INF/assets/scripts/sampleData/SampleCourses.js
@@ -5,34 +5,34 @@ app.service('SampleCourses', function() {
     context_title: "Potions 101",
     context_label: "ptns101",
     resource_link_id: "f81f1291-c0d2-483a-aa65-2696a614d50d",
-    resource_title: "Constructing a Convincing Polyjuice Potion"
+    resource_link_title: "Constructing a Convincing Polyjuice Potion"
   },
   {
     context_id: "b0933467-3d57-4ea8-98af-090e9e25e32f",
     context_title: "Defense Against the Dark Arts 101",
     context_label: "dada101",
     resource_link_id: "08b2765e-e30c-45ef-877a-5ba4e8cbf06e",
-    resource_title: "Perfecting a Patronus Charm"
+    resource_link_title: "Perfecting a Patronus Charm"
   },
   {
     context_id: "57c56f00-b67f-417a-8b9d-71b51e358974",
     context_title: "Herbology 101",
     context_label: "herb101",
     resource_link_id: "6e189877-32aa-475f-b664-7594ba262b3f",
-    resource_title: "Recognizing a Fanged Geranium"
+    resource_link_title: "Recognizing a Fanged Geranium"
   },
   {
     context_id: "3f9ead91-c2cb-4572-9c23-15a6c126f038",
     context_title: "Transfiguration 101",
     context_label: "tfgn101",
     resource_link_id: "6ff8de3a-b901-4208-bd2b-182db6ca7094",
-    resource_title: "Introduction to Vanishment"
+    resource_link_title: "Introduction to Vanishment"
   },
   {
     context_id: "7fc88522-27dc-411b-ae6f-88fec59dd448",
     context_title: "Charms 101",
     context_label: "chrm101",
     resource_link_id: "9ccbfe84-64b8-44d6-ae74-14df7da895e8",
-    resource_title: "Preparing for the O.W.L."
+    resource_link_title: "Preparing for the O.W.L."
   }];
 });
